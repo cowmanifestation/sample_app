8d8d3ee8c7a2ad7e523cc9de6561dec3a3b8771c
Cowmanifestation <cowmanifestation@gmail.com>
2012-07-03 13:18:41 -0700

    Finish sign in

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -55,8 +55,9 @@ describe "UserPages" do
         before {click_button submit }
         let(:user) { User.find_by_email('user@example.com') }
 
-        it {should have_selector('title', text: user.name) }
-        it {should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_selector('title', text: user.name) }
+        it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+        it { should have_link('Sign out') }
       end
     end
   end
